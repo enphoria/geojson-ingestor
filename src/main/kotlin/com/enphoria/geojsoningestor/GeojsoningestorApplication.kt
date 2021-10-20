5abb0fa785bcd99d1f4b9841c386e2d161b05af7
Camilo Cardenas <camilo.cardenas@mercadolibre.com>
2021-10-19 19:12:07 -0500

    se adiciona maven al proyecto con kotlin

---
+++ src/main/kotlin/com/enphoria/geojsoningestor/GeojsoningestorApplication.kt
@@ -0,0 +1,11 @@
+package com.enphoria.geojsoningestor
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class GeojsoningestorApplication
+
+fun main(args: Array<String>) {
+	runApplication<GeojsoningestorApplication>(*args)
+}
---
+++ src/test/kotlin/com/enphoria/geojsoningestor/GeojsoningestorApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.enphoria.geojsoningestor
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class GeojsoningestorApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
